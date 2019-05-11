a08ea15e181c4221065be297f6c9592cb69d1c19
columbian-ath <rukzak16me@gmail.com>
2019-05-11 19:03:27 +0300

    Add files via upload

---
+++ lab4_var13/30_bit/cmake-build-debug/CMakeFiles/30_bit.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/30_bit.dir/main.cpp.o"
+  "30_bit.pdb"
+  "30_bit"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/30_bit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
